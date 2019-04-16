753f28b2f7fa948c45597fa69cacf8002c3f66ae
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-04-15 21:28:53 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(response => response.json())
+    .then(({people}) => this.setState({ data: people }))
+  }
+
+  render() {
+    return (
+      <div>
+      {this.state.data.map((person) => <p>{person.name}</p>)}
+      </div>
+    )
+  }
+}
